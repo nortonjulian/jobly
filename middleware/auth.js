327dacffee5b5a24c100287d42802d67a8ad69b7
Julian <nortonjulian@gmail.com>
2023-07-19 15:56:17 -0600

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -49,20 +49,22 @@ function ensureAdmin(req, res, next) {
 function ensureCorrectUserOrAdmin(req, res, next) {
   try {
     const user = res.locals.user;
-    const requestedUsername = req.params.username
+    const requestedUsername = req.params.username;
 
     console.log("User:", user);
     console.log("Requested Username:", requestedUsername);
 
     // Check if user is an admin or the same user as requested
     if (!(user && (user.isAdmin || user.username === requestedUsername))) {
-      throw new UnauthorizedError();
+      throw new UnauthorizedError(); // Throw an UnauthorizedError when user is unauthorized
     }
-    return next();
+
+    return next(); // Call next() only when user is authorized
   } catch (err) {
-    return next(err);
+    return next(err); // Call next() with the error when UnauthorizedError is thrown
   }
 }
+
 
 module.exports = {
   authenticateJWT,
--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -3,45 +3,56 @@ const { ensureCorrectUserOrAdmin } = require("../middleware/auth");
 
 describe("ensureCorrectUserOrAdmin", function () {
   test("works: admin", function () {
-    expect.assertions(1);
     const req = { params: { username: "test" } };
     const res = { locals: { user: { username: "admin", isAdmin: true } } };
-    const next = function (err) {
-      expect(err).toBeFalsy();
-    };
+    const next = jest.fn();
+
     ensureCorrectUserOrAdmin(req, res, next);
+
+    // Ensure next middleware function is called
+    expect(next).toHaveBeenCalled();
+    // Ensure no error is thrown
+    expect(next.mock.calls[0][0]).toBeFalsy();
   });
 
   test("works: same user", function () {
-    expect.assertions(1);
     const req = { params: { username: "test" } };
     const res = { locals: { user: { username: "test", isAdmin: false } } };
-    const next = function (err) {
-      expect(err).toBeFalsy();
-    };
+    const next = jest.fn();
+
     ensureCorrectUserOrAdmin(req, res, next);
+
+    // Ensure next middleware function is called
+    expect(next).toHaveBeenCalled();
+    // Ensure no error is thrown
+    expect(next.mock.calls[0][0]).toBeFalsy();
   });
 
   test("unauth: mismatch", function () {
-    expect.assertions(1);
     const req = { params: { username: "test" } };
     const res = { locals: { user: { username: "otheruser", isAdmin: false } } };
-    const next = function (err) {
-      expect(err instanceof UnauthorizedError).toBeTruthy();
-    };
+    const next = jest.fn();
+
     ensureCorrectUserOrAdmin(req, res, next);
+
+    // Ensure next middleware function is not called
+    expect(next).not.toHaveBeenCalled();
+    // Ensure UnauthorizedError is thrown
+    expect(next.mock.calls.length).toBe(1);
+    expect(next.mock.calls[0][0] instanceof UnauthorizedError).toBeTruthy();
   });
 
   test("unauth: if not admin", function () {
-    expect.assertions(1);
     const req = { params: { username: "test" } };
     const res = { locals: { user: { username: "test", isAdmin: false } } };
-    const next = function (err) {
-      console.log("Error:", err);
-      expect(err instanceof UnauthorizedError).toBeTruthy();
-    };
-    console.log("res.locals.user:", res.locals.user);
-    console.log("req.params.username:", req.params.username);
+    const next = jest.fn();
+
     ensureCorrectUserOrAdmin(req, res, next);
+
+    // Ensure next middleware function is not called
+    expect(next).not.toHaveBeenCalled();
+    // Ensure UnauthorizedError is thrown
+    expect(next.mock.calls.length).toBe(1);
+    expect(next.mock.calls[0][0] instanceof UnauthorizedError).toBeTruthy();
   });
 });
